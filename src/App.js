301f193f3e27c61aa4ee3784f4ff9b1f66571508
waqaxhp <waqaxhp@gmail.com>
2024-10-12 23:40:55 +0500

    Loogo Updated !
    Loogo Updated !

--- src/App.js
+++ src/App.js
@@ -1,28 +1,4 @@
-// import './App.css'
-// import Navbar from './Components/Navebar/Navebar.jsx'
-// import Home from './Components/Home/Home.jsx'
-// // import AllComponents from './Components/MergeAllComponents/MergeAllComponents.jsx'
-// import Footer from './Components/Footer/Footer.jsx'
-// import Projects from './Components/Projects/Projects.jsx'
-// import Services from './Components/Services/Services.jsx'
-// import Consulting from './Components/Consulting/Consulting.jsx'
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navbar />
-//       <Home />
-//       <Services/>
-//       <Projects/>
-//       {/* <AllComponents /> */}
-//       <Consulting/>
-//       <Footer />
-//     </div>
-//   )
-// }
-
-// export default App
-// App.js
 import './App.css'
 import Navbar from './Components/Navebar/Navebar.jsx'
 import Home from './Components/Home/Home.jsx'
