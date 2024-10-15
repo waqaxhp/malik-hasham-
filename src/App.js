e9c6b9477db7fbebb9467086c4f0021eb4d2511b
waqaxhp <waqaxhp@gmail.com>
2024-10-15 22:03:08 +0500

    Navlinks Are Added !

--- src/App.js
+++ src/App.js
@@ -1,31 +1,81 @@
 
-import './App.css'
-import Navbar from './Components/Navebar/Navebar.jsx'
-import Home from './Components/Home/Home.jsx'
-// import Hero from './Components/Hero/Hero.jsx'
-import Footer from './Components/Footer/Footer.jsx'
-import Projects from './Components/Projects/Projects.jsx'
-import Services from './Components/Services/Services.jsx'
-import Consulting from './Components/Consulting/Consulting.jsx'
-import ContactUs from './Components/ContactUS/ContactUs.jsx'
-import Quality from './Components/Quality/Quality.jsx'
-import Consultants from './Components/Consultants/Consultants.jsx'
+// import './App.css'
+// import Navbar from './Components/Navebar/Navebar.jsx'
+// import Home from './Components/Home/Home.jsx'
+// import Footer from './Components/Footer/Footer.jsx'
+// import Projects from './Components/Projects/Projects.jsx'
+// import Services from './Components/Services/Services.jsx'
+// import Consulting from './Components/Consulting/Consulting.jsx'
+// import ContactUs from './Components/ContactUS/ContactUs.jsx'
+// import Quality from './Components/Quality/Quality.jsx'
+// import Consultants from './Components/Consultants/Consultants.jsx'
 
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* Navbar */}
+//       <Navbar />
+      
+//       {/* All sections on Home Page */}
+//       <section id="home">
+//         <Home />
+//       </section>
+
+//       {/* <section id="hero">
+//         <Hero />
+//       </section> */}
+
+//       <section id="services">
+//         <Services />
+//       </section>
+
+//       <section id="projects">
+//         <Projects />
+//       </section>
+
+//       <section id='quality'>
+//         <Quality/>
+//       </section>
+      
+//       <section id="consultants">
+//         <Consultants />
+//       </section> 
+
+//       <section id="consulting">
+//         <Consulting />
+//       </section>
+
+//       <section id="contactUs">
+//         <ContactUs/>
+//       </section>
+
+//       {/* Footer */}
+//       <Footer />
+//     </div>
+//   )
+// }
+
+// export default App
+import './App.css';
+import Navbar from './Components/Navebar/Navebar.jsx';
+import Home from './Components/Home/Home.jsx';
+import Footer from './Components/Footer/Footer.jsx';
+import Projects from './Components/Projects/Projects.jsx';
+import Services from './Components/Services/Services.jsx';
+import Consulting from './Components/Consulting/Consulting.jsx';
+import ContactUs from './Components/ContactUS/ContactUs.jsx';
+import Quality from './Components/Quality/Quality.jsx';
+import Consultants from './Components/Consultants/Consultants.jsx';
 
 function App() {
   return (
     <div className="App">
-      {/* Navbar */}
       <Navbar />
       
-      {/* All sections on Home Page */}
       <section id="home">
         <Home />
       </section>
-
-      {/* <section id="hero">
-        <Hero />
-      </section> */}
 
       <section id="services">
         <Services />
@@ -47,14 +97,13 @@ function App() {
         <Consulting />
       </section>
 
-      <section id="ContactUs">
+      <section id="contactUs">
         <ContactUs/>
       </section>
 
-      {/* Footer */}
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
